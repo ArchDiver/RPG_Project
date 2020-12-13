5d44bae89bf187425b46ac725e1c6945ce90fabb
LSC <lelandscrawford@gmail.com>
2020-12-13 18:59:12 -0500

    Dec13

--- game.py
+++ game.py
@@ -529,3 +529,4 @@ def setup_game():
     print("You find yourself in the center of a strange place.\nSeems like you are trapped in a little box.\n"
     print("Every inside face of the box seems to have a different theme.\nHow can you get out of this...\n")
     print("You notice objects standing sideways on the walls.\nDoes gravity not apply? There are clouds though...")
+    main_game_loop()
